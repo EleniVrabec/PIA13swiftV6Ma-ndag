f5e2c6a3a45bff2c03eabc4ddd2383f594fddaeb
EleniVrabec <eleni.vrabec@gritacademy.se>
2024-12-09 09:10:43 +0100

    Initial Commit

---
+++ PIA13swiftV6Måndag/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PIA13swiftV6Måndag
+//
+//  Created by Eleni Vrabec on 2024-12-09.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PIA13swiftV6Måndag/PIA13swiftV6Ma_ndagApp.swift
@@ -0,0 +1,17 @@
+//
+//  PIA13swiftV6Ma_ndagApp.swift
+//  PIA13swiftV6Måndag
+//
+//  Created by Eleni Vrabec on 2024-12-09.
+//
+
+import SwiftUI
+
+@main
+struct PIA13swiftV6Ma_ndagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
